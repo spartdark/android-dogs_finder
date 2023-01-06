ac0b8f790d4dd00018265c92bbf839b989ff860a
Vladimir Saldivar <vladimir.saldivar@morsco.com>
2023-01-06 16:45:55 -0600

    dog detail view added

---
+++ app/src/main/java/com/vsaldivarm/dogsdex/dogdetail/DogDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.vsaldivarm.dogsdex.dogdetail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vsaldivarm.dogsdex.R
+
+class DogDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dog_detail)
+    }
+}
