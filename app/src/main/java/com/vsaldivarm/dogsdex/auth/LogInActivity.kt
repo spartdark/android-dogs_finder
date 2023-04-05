60a2483b3caa6650217f0c204fbfaef4499bcc6b
Vladimir Saldivar <vsaldivarm@gmail.com>
2023-04-04 23:51:55 -0600

    i created the login and siginup fragments

---
+++ app/src/main/java/com/vsaldivarm/dogsdex/auth/LogInActivity.kt
@@ -0,0 +1,12 @@
+package com.vsaldivarm.dogsdex.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vsaldivarm.dogsdex.R
+
+class LogInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+    }
+}
