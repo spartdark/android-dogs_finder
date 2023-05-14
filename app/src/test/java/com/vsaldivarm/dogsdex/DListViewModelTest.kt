e7400296d2b894f3d1d5ea868a5e2112b6223952
Vladimir Saldivar <vsaldivarm@gmail.com>
2023-05-13 20:46:59 -0600

    adding test

---
+++ app/src/test/java/com/vsaldivarm/dogsdex/DListViewModelTest.kt
@@ -0,0 +1,20 @@
+package com.vsaldivarm.dogsdex
+
+import com.vsaldivarm.dogsdex.doglist.DogListViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Rule
+import org.junit.Test
+
+class DListViewModelTest {
+@ExperimentalCoroutinesApi
+@get:Rule
+var dogedexCoRule = MainCoroutineRule()
+
+
+    @Test
+    fun downloadDogs(){
+        val viewModel : DogListViewModel(
+        dog
+        )
+    }
+}
